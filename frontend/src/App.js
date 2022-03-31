30b1c8859f778005e9f9a10b987137ba6b7e7354
Dylan Jung <Dj5822@gmail.com>
2022-03-31 17:46:44 +1300

    Setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import AuthenticationPage from './pages/AuthenticationPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" >
+            <Route index element={<Navigate to="/auth" />} />
+            <Route path="home" element={<Homepage />} />
+            <Route path="auth" element={<AuthenticationPage />} />
+            <Route path="*" element={<div>Page not found.</div>} />
+          </Route>
+        </Routes>
+      </div>
+      </BrowserRouter>
   );
 }
 
