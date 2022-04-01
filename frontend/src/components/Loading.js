e2b34990989d90e4cec55b1dac6b3665af4130b8
Dylan Jung <Dj5822@gmail.com>
2022-04-01 22:03:33 +1300

    Styled loading screen

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,9 +1,13 @@
+import { CircularProgress } from "@mui/material";
 import React from "react";
+import classes from "./Loading.module.css";
 
-const Loading = () => (
-    <div>
-      Loading...
+const Loading = () => {
+  return (
+    <div className={classes.background} >
+      <CircularProgress size={120} />
     </div>
-  );
+  )
+};
   
-  export default Loading;
+export default Loading;
