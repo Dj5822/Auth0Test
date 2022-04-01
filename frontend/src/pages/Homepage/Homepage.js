8989f28553559994a237096935c11a14119ffe6d
Dylan Jung <Dj5822@gmail.com>
2022-04-01 23:06:24 +1300

    Modified index.html

--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Button, Typography, Avatar } from '@mui/material';
-import { useAuth0 } from "@auth0/auth0-react";
 import MessagePanel from '../../components/MessagePanel/MessagePanel';
 import UserDetailsPanel from '../../components/UserDetailsPanel/UserDetailsPanel';
 
